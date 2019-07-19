c678fb270dd45f4d468e7a7d39bdc5c0fd72a5bf
Snow <polarpenguin@foxmail.com>
2019-07-19 16:18:09 +0800

    For ACM contest demo purpose

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
+// https://github.com/penguin806/GtfFilePreprocessor.git
 #include "snowmainwnd.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    const QString applicationName =
+            QString::fromLocal8Bit("高通量多组学序列数据可视化浏览器 - 数据预处理");
     QApplication a(argc, argv);
-    QApplication::setApplicationName("GtfFilePreprocessor");
+    // QApplication::setApplicationName("GtfFilePreprocessor");
+    QApplication::setApplicationName(applicationName);
     QApplication::setApplicationVersion("1.0.0");
     QApplication::setOrganizationDomain("xuefeng.space");
     SnowMainWnd w;
+    w.setWindowTitle(applicationName);
     w.show();
 
     return a.exec();
