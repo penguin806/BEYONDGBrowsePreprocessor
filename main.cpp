39308a3e68944ea6b6ce4ab0e33057101c6132d8
Snow <polarpenguin@foxmail.com>
2019-08-27 01:34:12 +0800

    Set text codec to GBK

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 // https://github.com/penguin806/GtfFilePreprocessor.git
 #include "snowmainwnd.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+    QTextCodec *codec = QTextCodec::codecForName("GBK");
+    QTextCodec::setCodecForLocale(codec);
     const QString applicationName =
             QString::fromLocal8Bit("高通量多组学序列数据可视化浏览器 - 数据预处理");
     QApplication a(argc, argv);
